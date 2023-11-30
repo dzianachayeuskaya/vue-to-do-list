30425455ae85c10da5bd294f884f88e502863f42
dziana chayeuskaya <96259458+dzianachayeuskaya@users.noreply.github.com>
2023-11-30 10:19:58 +0100

    Updates

---
+++ _nuxt/00c0e21.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{1265:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},o=t(89),component=Object(o.a)(l,(function(){return(0,this._self._c)("MainPage")}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{MainPage:t(1263).default})}}]);
